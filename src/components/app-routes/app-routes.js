14b2e32d0eeef9e4bc8fa906a6cf678f201e6a03
saif <saif@unicode.ai>
2021-03-26 20:30:03 +0300

    admin stuff

--- src/components/app-routes/app-routes.js
+++ src/components/app-routes/app-routes.js
@@ -1,27 +1,28 @@
 import React from 'react';
-import LoginPage from "../LoginPage/Login";
-import Register from "../registration/registration";
+import LoginPage from '../LoginPage/Login';
+import Register from '../registration/registration';
 import {Route, Switch, BrowserRouter, Redirect} from 'react-router-dom';
 import AddItem from '../addItem/addItem';
+import Home from '../home/home';
 
 const AppRoutes = () => {
 
-
-    return (
-        <main>
-            <BrowserRouter>
-            <Switch>
-                <Route exact path={'/login'} component={LoginPage}/>
-                <Route exact path='/register' component={Register} redirect={false}/>
-              {localStorage.getItem("user")?
-                  <Route exact path='/add-item' component={AddItem} redirect={false}/>
-              :
-                  <Redirect to='/login'/>
-              }
-            </Switch>
-            </BrowserRouter>
-        </main>
-    )
+  return (
+      <main>
+        <BrowserRouter>
+          <Switch>
+            <Route exact path={'/'} component={Home}/>
+            <Route exact path={'/login'} component={LoginPage}/>
+            <Route exact path='/register' component={Register} redirect={false}/>
+            {localStorage.getItem('user') && localStorage.getItem('user') === 'hWGZRo9Zf0QXYUSJGmLAMIAiouD2' ?
+                <Route exact path='/add-item' component={AddItem} redirect={false}/>
+                :
+                <Redirect to='/login'/>
+            }
+          </Switch>
+        </BrowserRouter>
+      </main>
+  );
 };
 
 export default AppRoutes;
---
+++ src/components/home/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Home = () => {
+  return (
+      <div style={{display:'flex',alignItems:'center',justifyContent:'center'}}>
+        <h1>
+          Welcome
+        </h1>
+      </div>
+  );
+};
+
+export default Home;
